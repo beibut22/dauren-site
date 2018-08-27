22651e27fad0335a18a6df6bccb9fad2bcf2c4c5
alexanderzhilyaev <sassoftinc@gmail.com>
2018-08-27 23:49:06 +0200

    install command bus and yii2-user

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require __DIR__.'/params.php';
 $db = require __DIR__.'/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => '1biz',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -12,26 +12,27 @@ $config = [
         '@npm' => '@vendor/npm-asset',
     ],
     'components' => [
+        'commandBus' => [
+            'class' => 'trntv\bus\CommandBus',
+            'locator' => [
+                'class' => 'trntv\bus\locators\ClassNameLocator',
+                'handlers' => [
+                    'app\commands\SomeCommand' => 'app\handlers\SomeHandler',
+                ],
+            ],
+        ],
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'LK_gA0NkkZwg9OYsJ8pzRLMc4alOBkiL',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -43,14 +44,39 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                'index' => 'site/index',
+                'login' => 'user/security/login',
+                'register' => 'user/registration/register',
+                'recover' => 'user/recovery/request',
+                'logout' => 'site/logout',
+
+                'admin' => 'admin/index',
             ],
         ],
-        */
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@dektrium/user/views' => '@app/views/user',
+                ],
+            ],
+        ],
+
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'admins' => ['admin'],
+            'enableConfirmation' => false,
+            'urlRules' => [],
+            'controllerMap' => [
+                'registration' => 'app\controllers\user\RegistrationController',
+            ],
+        ],
     ],
     'params' => $params,
 ];
